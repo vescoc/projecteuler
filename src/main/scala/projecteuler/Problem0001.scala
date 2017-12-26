10176462f28d6c60f19aa572c789f97010826516
vescoc <claudio.vesco@gmail.com>
2017-12-26 11:50:11 +0100

    solved

--- src/main/scala/projecteuler/Problem0001.scala
+++ src/main/scala/projecteuler/Problem0001.scala
@@ -1,7 +1,17 @@
 package projecteuler
 
-object Problem0001 {
-  def main(args: Array[String]) {
+import scala.annotation.tailrec
 
+object Problem0001 {
+  @tailrec
+  def solve(n: Int = 1, current: Int = 0, limit: Int = 10): Int =
+    if (n == limit)
+      current
+    else
+      solve(n + 1, current + (if (n % 3 == 0 || n % 5 == 0) n else 0), limit)
+
+  def main(args: Array[String]) {
+    println("test: " + solve())
+    println("problem: " + solve(limit = 1000))
   }
 }

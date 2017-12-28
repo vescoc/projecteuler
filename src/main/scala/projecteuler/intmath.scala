bc674c574d307ca5117b27847fbbb832dd65b7e9
vescoc <claudio.vesco@gmail.com>
2017-12-28 16:30:24 +0100

    using integral

--- src/main/scala/projecteuler/intmath.scala
+++ src/main/scala/projecteuler/intmath.scala
@@ -3,48 +3,77 @@ package projecteuler
 import scala.annotation.tailrec
 
 trait IntMathTrait {
-  lazy val prime: Stream[Int] = 1 #:: 2 #:: {
-    def next(n: Int, current: Int): Stream[Int] = {
-      @tailrec
-      def find(current: Int = current + 1): Int = {
-        val half = current / 2
+  lazy val prime: Stream[Int] = Prime[Int]
 
-        @tailrec
-        def check(s: Stream[Int] = prime.tail): Boolean = {
-          val head = s.head
-          if (head > half)
-            true
-          else if (current % head == 0)
-            false
-          else
-            check(s.tail)
-        }
+  object Prime {
+    private var map: Map[Any, Any] = Map()
 
-        if (check())
-          current
-        else
-          find(current + 1)
-      }
-
-      val p = find()
-
-      p #:: next(n + 1, p)
+    def apply[T : Integral](implicit m: Manifest[T]): Stream[T] = {
+      if (!map.contains(m))
+        map = map + (m -> prime[T])
+      map(m).asInstanceOf[Stream[T]]
     }
 
-    next(2, 2)
+    def prime[T : Integral]: Stream[T] = {
+      val numeric = implicitly[Integral[T]]
+
+      val zero = numeric.zero
+      val one = numeric.one
+      val two = numeric.plus(numeric.one, numeric.one)
+
+      lazy val p: Stream[T] = one #:: two #:: {
+        def next(n: Int, current: T): Stream[T] = {
+          @tailrec
+          def find(current: T = numeric.plus(current, one)): T = {
+            val half = numeric.quot(current, two)
+
+            @tailrec
+            def check(s: Stream[T] = p.tail): Boolean = {
+              val head = s.head
+              if (numeric.gt(head, half))
+                true
+              else if (numeric.equiv(numeric.rem(current, head), zero))
+                false
+              else
+                check(s.tail)
+            }
+
+            if (check())
+              current
+            else
+              find(numeric.plus(current, one))
+          }
+
+          val v = find()
+
+          v #:: next(n + 1, v)
+        }
+
+        next(2, two)
+      }
+
+      p
+    }
   }
 
-  def factor(n: Int) = {
+  def factor[T : Integral](n: T)(implicit m: Manifest[T]) = {
+    val numeric = implicitly[Integral[T]]
+
+    val prime = Prime.apply
+
+    val zero = numeric.zero
+    val one = numeric.one
+
     @tailrec
-    def factor(idx: Int = 1, n: Int = n, f: Int = 0, current: Seq[(Int, Int)] = List((1 -> 1))): Seq[(Int, Int)] = {
+    def factor(idx: Int = 1, n: T = n, f: Int = 0, current: Seq[(T, Int)] = List((one -> 1))): Seq[(T, Int)] = {
       val p = prime(idx)
-      if (n <= 1) {
+      if (numeric.lteq(n, one)) {
         if (f == 0)
           current
         else
           (p -> f) +: current
-      } else if (n % p == 0)
-        factor(idx, n / p, f + 1, current)
+      } else if (numeric.equiv(numeric.rem(n, p), zero))
+        factor(idx, numeric.quot(n, p), f + 1, current)
       else if (f == 0)
         factor(idx + 1, n, 0, current)
       else
@@ -67,22 +96,25 @@ trait IntMathTrait {
     p()
   }
   
-  def mcm(numbers: Seq[Int]) = {
+  def mcm[T : Integral](numbers: Seq[T])(implicit m: Manifest[T]) = {
+    val numeric = implicitly[Integral[T]]
+
     @tailrec
-    def mcm(numbers: Seq[Int] = numbers, current: Map[Int, Int] = Map()): Map[Int, Int] =
+    def mcm(numbers: Seq[T] = numbers, current: Map[T, Int] = Map()): Map[T, Int] =
       if (numbers.size == 0)
         current
       else {
         val head = numbers.head
         val merge = {
           val map = factor(head).toMap
+
           (map.keySet ++ current.keySet).map(k => (k -> math.max(map.getOrElse(k, 0), current.getOrElse(k, 0)))).toMap
         }
 
         mcm(numbers.tail, merge)
       }
 
-    mcm().foldLeft(1)((r, c) => r * pow(c._1, c._2))
+    mcm().foldLeft(numeric.one)((r, c) => numeric.times(r, pow(c._1, c._2)))
   }
 }
 
